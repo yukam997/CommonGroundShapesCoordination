f7659d5e1eac36168b38013f067ad85e401a5859
Yuka <myuhka12@gmail.com>
2026-02-15 11:52:22 -0800

    adding all tracked files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Yu2024_replication
+
+attention_check.r creates "games_for_analysis.csv" which is what passed attention checks.
+gather_files.r gathers all relevant data because doing experiment in one file on empirica is too big and creates issues.
+count_strategy for counting strategy
+rest of analysis is in statsForCogsci
--- prolific_management/gather_files.r
+++ prolific_management/gather_files.r
@@ -1,44 +1,45 @@
 library(dplyr)
 library(ggplot2)
-directories <- c(
-    "../Yu2024_replication-2026-01-20-21-00-29",
-    "../Yu2024_replication-2026-01-20-21-59-57",
-    "../Yu2024_replication-2026-01-20-23-54-50"
-)
-#set directory as same as this r file location
+
 working_directory <- file.path(dirname(rstudioapi::getActiveDocumentContext()$path))
 setwd(working_directory)
+directories <- list.dirs(path = "../", full.names = TRUE, recursive = FALSE)
+# Files were collected in multiple rounds to avoid overloading the server files. These were stored as real_data_1, real_data_2, etc. We only want to read those directories.
+directories <- directories[grepl("real_data_", directories)]
+
 # concatenate ./game.csv files from each directory
 game_df <- do.call(rbind, lapply(directories, function(dir) {
     read.csv(file.path(dir, "game.csv"))
 }))
 
-player_df <- do.call(rbind, lapply(directories, function(dir) {
-    read.csv(file.path(dir, "player.csv"))
-}))
-# save file as csv
-# filter out games where endedReason is not "end of game"
+# filter out games where endedReason is not "end of game". Games that remain are all the ones which finished playing.
 game_df <- game_df[game_df$endedReason == "end of game", ]
-# there are duplicate rows
-game_df <- game_df[!duplicated(game_df), ]
-player_df <- player_df[player_df$gameID %in% game_df$id, ]
-# remove columns if they happen twice in this case, take the later one because it would have happend in the second folder and first
-player_df <- player_df[!duplicated(player_df$id,fromLast = TRUE), ]
-write.csv(game_df, file.path(working_directory, "../data_analysis/combined_game.csv"), row.names = FALSE)
-write.csv(player_df, file.path(working_directory, "../data_analysis/combined_player.csv"), row.names = FALSE)
+for (tName in unique(game_df$treatmentName)) {
+    n_games <- nrow(game_df[game_df$treatmentName == tName, ])
+    print(paste("Treatment:", tName, "Number of completed games:", n_games))
+}
+write.csv(game_df, file.path(working_directory, "../data_analysis/data_from_jan27_full/combined_game.csv"), row.names = FALSE)
 
-playerStage_df <- do.call(rbind, lapply(directories, function(dir) {
+# we store different csv files for each game, player, playerStage, and playerRound information, making sure we filter out games which didn't finish.
+player_df <- lapply(directories, function(dir) {
+    read.csv(file.path(dir, "player.csv"))
+})
+player_df <- bind_rows(player_df)
+player_df <- player_df[player_df$gameID %in% game_df$id, ]
+write.csv(player_df, file.path(working_directory, "../data_analysis/data_from_jan27_full/combined_player.csv"), row.names = FALSE)
+
+playerStage_df <- lapply(directories, function(dir) {
     read.csv(file.path(dir, "playerStage.csv"))
-}))
+}) 
+playerStage_df <- bind_rows(playerStage_df)
 playerStage_df <- playerStage_df[playerStage_df$playerID %in% player_df$id, ]
-playerStage_df <- playerStage_df[!duplicated(playerStage_df), ]
-playerStage_df <- playerStage_df[!duplicated(playerStage_df$id,fromLast = TRUE), ]
-# save playerStage_df as csv
-write.csv(playerStage_df, file.path(working_directory, "../data_analysis/combined_playerStage.csv"), row.names = FALSE)
-playerRound_df <- do.call(rbind, lapply(directories, function(dir) {
-    read.csv(file.path(dir, "playerRound.csv"))
-}))
+
+write.csv(playerStage_df, file.path(working_directory, "../data_analysis/data_from_jan27_full/combined_playerStage.csv"), row.names = FALSE)
+
+playerRound_df <- lapply(directories, function(dir) {
+    read.csv(file.path(dir, "playerRound.csv"))}) %>% 
+    bind_rows()
+sum(duplicated(playerRound_df))
 playerRound_df <- playerRound_df[playerRound_df$playerID %in% player_df$id, ]
-playerRound_df <- playerRound_df[!duplicated(playerRound_df), ]
-playerRound_df <- playerRound_df[!duplicated(playerRound_df$id,fromLast = TRUE), ]
-write.csv(playerRound_df, file.path(working_directory, "../data_analysis/combined_playerRound.csv"), row.names = FALSE)
+write.csv(playerRound_df, file.path(working_directory, "../data_analysis/data_from_jan27_full/combined_playerRound.csv"), row.names = FALSE)
+
